9125113b0149a6dd89dca3dd2b759a684d4f8e7c
JezzBz <63681024+JezzBz@users.noreply.github.com>
2022-02-08 19:53:08 +0300

    FirstCommitForDeploy

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function HomePage() {
+  return <div>
+     <script async src="https://telegram.org/js/telegram-widget.js?15" data-telegram-login="CoopGalacticBot" data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
+  </div>;
+}
