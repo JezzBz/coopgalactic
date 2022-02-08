7b1a7239eb3e44833c5c9bef3e556ddbae6bc742
JezzBz <63681024+JezzBz@users.noreply.github.com>
2022-02-08 20:35:28 +0300

    Test

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import axios from 'axios';
 export default function Header() {
     
+    
     const GetTelegramOAuth=()=>{
-        axios.get("https://telegram.org/js/telegram-widget.js?15%22%20data-telegram-login=%22CoopGalacticBot%22&data-size=large&data-onauth=onTelegramAuth(user)&data-request-access=write").then((req)=>{console.log(req)})
+        axios.post("https://oauth.telegram.org/auth/get?bot_id=547043436&lang=ru").then((req)=>{console.log(req)})
     }
     
   return <div>
