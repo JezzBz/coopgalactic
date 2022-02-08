9b223e60ec73abc8b6ac040a4d2f031241e0adaa
JezzBz <63681024+JezzBz@users.noreply.github.com>
2022-02-08 19:57:51 +0300

    SecondCommit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,32 +5,12 @@ export default function Header() {
     const GetTelegramOAuth=()=>{
         axios.get("https://telegram.org/js/telegram-widget.js?15%22%20data-telegram-login=%22CoopGalacticBot%22&data-size=large&data-onauth=onTelegramAuth(user)&data-request-access=write").then((req)=>{console.log(req)})
     }
-    <button onClick={GetTelegramOAuth}>
-
-    </button>
+    
   return <div>
 
+<button onClick={GetTelegramOAuth}>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    </button>
 
   </div>;
 }
